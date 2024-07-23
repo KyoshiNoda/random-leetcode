524ec7051bd46cc84d1227b2a20fb9b6248e9b85
kyoshinoda <kyoshisew@gmail.com>
2024-07-22 20:42:54 -0400

    wip before we switch to next

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color1: "#000428",
+        color2: "#004e92",
+      },
+    },
   },
   plugins: [],
-}
-
+};
