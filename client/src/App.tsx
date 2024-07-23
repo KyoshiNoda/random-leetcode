c2637994d8e20b71090805d13210c9d17e0ffa84
kyoshinoda <kyoshisew@gmail.com>
2024-07-22 20:29:39 -0400

    added frontend boiler

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen w-screen bg-cyan-300 items-center justify-center">
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
